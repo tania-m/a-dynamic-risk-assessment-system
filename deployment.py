f7e80cb81d0ae0baeb96c352b77cf192e345a8a2
Tania M <tania.mathern@gmail.com>
2024-01-15 14:14:19 -0800

    Deployment script

--- deployment.py
+++ deployment.py
@@ -24,6 +24,9 @@ with open('config.json','r') as f:
 dataset_csv_path = os.path.join(config['output_folder_path'])
 print(f"Dataset used source folder: {dataset_csv_path}")
 
+model_path = os.path.join(config['output_model_path']) 
+print(f"Model files source folder: {model_path}")
+
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
 print(f"Prod deployment folder: {prod_deployment_path}")
 
@@ -43,13 +46,25 @@ def move_deployment_files():
     ingestion_report_source_path = os.path.join(dataset_csv_path, ingestion_report_name)
     ingestion_report_target_path = os.path.join(prod_deployment_path, ingestion_report_name)
     print(f"Copying {ingestion_report_name} from {ingestion_report_source_path} to the deployment directory {prod_deployment_path}")
-    
     try:
         shutil.copy(ingestion_report_source_path, ingestion_report_target_path)
     except FileNotFoundError:
+        # prod_deployment_path folder may not be here at first run
         print("Target folder doesn't seem to existing. Creating it...")
         os.mkdir(prod_deployment_path)
         shutil.copy(ingestion_report_source_path, ingestion_report_target_path)
+        
+    model_files = os.listdir(model_path)
+    for data_file in model_files:
+        source_path = os.path.join(model_path, data_file)
+        target_path = os.path.join(prod_deployment_path, data_file)
+        # prod_deployment_path folder should exist now
+        # if this is the first run, the folder got created above
+        # if the folder is not here anymore, something is really off
+        # and we'll let it crash
+        print(f"Copying {source_path} to {target_path}")
+        shutil.copy(source_path, target_path)
+
 
 if __name__ == "__main__":
     move_deployment_files()
